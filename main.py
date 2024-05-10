14da51c81e8ada500a7b82808e770a751269a07e
alta7700 <alta7700@mail.ru>
2024-05-10 10:10:51 +0300

    добавил id-mode

--- main.py
+++ main.py
@@ -48,12 +48,19 @@ def fill_fio_layers(fio_lines: list[str]):
         lines_layers[line].textItem.contents = fio_lines[i]
 
 
-def main(lines: list[int], results_dir: Path, csv: Path):
+def main(lines: list[int], results_dir: Path, csv: Path, id_mode: bool):
     results_dir = results_dir.absolute()
     with open(csv, 'r', encoding='utf-8') as studs:
         studs.readline()  # read header
         for stud in studs:
-            user_id, course, group, *fio_lines = (x for x in stud.strip('\n').split(';') if x)
+            if not stud:
+                continue
+            if id_mode:
+                user_id, course, group, *fio_lines = (x for x in stud.strip('\n').split(';') if x)
+                out_filename = f'{user_id}.png'
+            else:
+                course, group, *fio_lines = (x for x in stud.strip('\n').split(';') if x)
+                out_filename = f'{str(group).zfill(3)}гр_{" ".join(fio_lines)}.png'
             if len(fio_lines) not in lines:
                 continue
             for layer in doc.artLayers:
@@ -65,7 +72,7 @@ def main(lines: list[int], results_dir: Path, csv: Path):
                             layer.textItem.contents = group
             fill_fio_layers(fio_lines)
 
-            png_file = results_dir / f'{user_id}.png'
+            png_file = results_dir / out_filename
             doc.saveAs(png_file.as_posix(), ps.PNGSaveOptions(), asCopy=True)
             print(png_file.name)
 
@@ -77,6 +84,7 @@ if __name__ == '__main__':
     parser.add_argument('--csv', type=Path, nargs='?', default=BASE_PATH / 'studs.csv')
     parser.add_argument('--results-dir', type=Path, nargs='?', default=BASE_PATH / 'results')
     parser.add_argument('--lines', type=int, nargs='*', default=[1, 2, 3, 4])
+    parser.add_argument('--id-mode', action='store_true', default=False)
     options = parser.parse_args().__dict__
     rd = options['results_dir']
     if not rd.exists():
